6990cb1d76858a12f2a6992c2ba573631e5ad7a7
Matt Fellows <matt.fellows@onegeek.com.au>
2020-04-09 16:14:40 +1000

    wip: add basic publisher test

---
+++ src/dsl/publisher.spec.ts
@@ -0,0 +1,23 @@
+/* tslint:disable:no-unused-expression no-empty no-string-literal*/
+import * as chai from "chai"
+import * as chaiAsPromised from "chai-as-promised"
+
+import { Publisher } from "./publisher"
+chai.use(chaiAsPromised)
+
+const expect = chai.expect
+
+describe.only("Publisher", () => {
+  describe("#constructor", () => {
+    it("constructs a valid Pubisher class", () => {
+
+      const p = new Publisher({
+        consumerVersion: "1.0.0",
+        pactBroker: "http://foo.com",
+        pactFilesOrDirs: [],
+      })
+      expect(p).to.not.be.null
+    })
+
+  })
+})
